937e3c63ea6b4dc83d6f8ab8a9ffa41632fa4b2b
alinemonteirocampelo <aline.campelo.monteiro@gmail.com>
2026-04-29 13:10:12 -0300

    laco-repeticao-for

---
+++ ex01/script.js
@@ -0,0 +1,7 @@
+let numero = Number(prompt("Digite um número:"));
+let mensagem = " ";
+
+for (let contador = 1; contador <= numero; contador ++){
+    mensagem += contador +" ";
+}
+alert(mensagem);
